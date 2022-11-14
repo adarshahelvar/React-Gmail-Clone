28f5501d848892bd45e3bbbcc74206fdd407494a
Adarsha A <adarshaadi1997@gmail.com>
2022-11-14 18:40:41 +0530

    Header Completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReorderIcon from '@mui/icons-material/Reorder';
-import { IconButton } from '@material-ui/core';
+import { IconButton, Avatar } from '@material-ui/core';
 import SearchIcon from '@mui/icons-material/Search';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AppsIcon from '@mui/icons-material/Apps';
 import "../css/header.css";
+
+
 const Header = () => {
   return (
     <div className='header'>
@@ -24,6 +29,19 @@ const Header = () => {
               <ExpandMoreIcon></ExpandMoreIcon>
             </IconButton>
           </div>
+        </div>
+
+        <div className='header__right'>
+          <IconButton>
+              <HelpOutlineIcon />
+          </IconButton>
+          <IconButton>
+            <SettingsIcon />
+          </IconButton>
+          <IconButton>
+            <AppsIcon />
+          </IconButton>
+          <Avatar src='#'/>
         </div>
     </div>
   )
