6b508477c678e5885a0e5a201ae54685596c5c57
Adarsha A <adarshaadi1997@gmail.com>
2022-11-15 11:50:00 +0530

    Body primary,social, promotins

--- src/components/EmailList.js
+++ src/components/EmailList.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../css/emailList.css';
 import EmailListSetting from './EmailListSetting';
+import EmailType from './EmailType';
 
 const EmailList = () => {
   return (
     <div className='emaillist'>
         <EmailListSetting />
+        <EmailType />
     </div>
   )
 };
